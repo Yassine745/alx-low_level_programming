220778e7065d7f971b1b6feaf90540d50881c894
Yassine745 <motorex745@gmail.com>
2023-06-15 15:50:46 +0000

    My first commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a qoute using write function 
+ *
+ * Return: 1 (error).
+*/
+int main(void)
+{       char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(2, quo, 59);	
+	return (1);
+}
